ba10935030c975d97617bd1127300c3e3983762e
Sruthi Krishna <sruthik926@gmail.com>
2019-01-20 03:37:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
+require 'pry'
+
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.count == 0
+   puts "The line is currently empty."
+  end
+  katz_deli.each do |name|
+    puts "The line is currently: " + katz_deli.index
+ 
+  end
+end
+
+def take_a_number(katz_deli, name)
+   puts "Welcome, " + name +  "You are number " + katz_deli.index(name) + "in line."
+end
