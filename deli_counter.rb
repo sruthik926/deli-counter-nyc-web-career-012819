0bec711f0d053920f500efe427712319f5b2ee61
Sruthi Krishna <sruthik926@gmail.com>
2019-01-20 03:25:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
    puts "The line is currently empty."
   end
   katz_deli.each do |name|
-    puts "The line is currently: " + katz_deli.index(name) + 1 + "." + name
+    puts "The line is currently: "
  
   end
 end
